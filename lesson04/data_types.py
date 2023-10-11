b7ec216340e7270c572148806f8962c31c5a1748
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-10-11 10:50:42 -0500

    finished with lesson04

--- lesson04/data_types.py
+++ lesson04/data_types.py
@@ -1,3 +1,4 @@
+import math
 # String data type
 
 # literal assignment
@@ -98,3 +99,37 @@ price = 100
 best_price = int(80)
 print(type(price))
 print(isinstance(best_price, int))
+
+# float type
+gpa = 3.28
+y = float(1.14)
+print(type(gpa))
+
+# complex type
+comp_value = 5+3j
+print(type(comp_value))
+print(comp_value.real)
+print(comp_value.imag)
+
+# Built-in functions for numbers
+
+print(abs(gpa))
+print(abs(gpa * -1))
+
+print(round(gpa))
+
+print(round(gpa, 1))
+
+
+print(math.pi)
+print(math.sqrt(64))
+print(math.ceil(gpa))
+print(math.floor(gpa))
+
+# Casting a string to a number
+zipcode = "10001"
+zip_value = int(zipcode)
+print(type(zip_value))
+
+# Error if you attempt to cast incorrect data
+# zip_value = int("New York")

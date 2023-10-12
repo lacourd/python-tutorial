08392cc6bf3a2681a60e2bd7016747bb2c4cdd78
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-10-12 15:10:33 -0500

    finished lesson09

--- lesson09/functions.py
+++ lesson09/functions.py
@@ -5,5 +5,27 @@ def hello_world():
 hello_world()
 
 
-def sum(num1, num2):
-    print(num1 + num2)
+def sum(num1=0, num2=0):
+    if (type(num1) is not int or type(num2) is not int):
+        return 0
+    return num1 + num2
+
+
+total = sum(7, 2)
+print(total)
+
+
+def multiple_items(*args):
+    print(args)
+    print(type(args))
+
+
+multiple_items("Dave", "John", "Sara")
+
+
+def mult_named_items(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+
+mult_named_items(first="Dave", last="Sara")

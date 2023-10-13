7b6848457b9d6c0803b1daa306ac40b479f322a2
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-10-13 11:16:34 -0500

    experiments with modules

--- lesson14/modules.py
+++ lesson14/modules.py
@@ -2,6 +2,7 @@ from math import pi
 import sys
 import random as rdm
 from enum import Enum
+import kansas
 
 
 print(pi)
@@ -9,3 +10,10 @@ rdm.choice("123")
 
 for item in dir(rdm):
     print(item)
+
+print(kansas.capital)
+kansas.randomfunfact()
+
+print(__name__)
+print(kansas.__name__)
+print(rdm.__name__)

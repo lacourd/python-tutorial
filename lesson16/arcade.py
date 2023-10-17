0ac4154c4691feaeac115102161e3a10169b2297
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-10-17 11:42:54 -0500

    finalized the arcade function

--- lesson16/arcade.py
+++ lesson16/arcade.py
@@ -9,16 +9,13 @@ def play_game(name='PlayerOne'):
         if (welcome_back == True):
             print(f"\n{name}, welcome back to the Arcade menu.")
 
-            playerchoice = input(
-            f"\n{name}, welcome to the arcade!\n\nPlease chooce a game:\n1 = Rock Paper Scissors\n2 = Guess My Number\n\nOr press \"x\" to exit the Arcade\n\n")
+        playerchoice = input(f"\n\nPlease chooce a game:\n1 = Rock Paper Scissors\n2 = Guess My Number\n\nOr press \"x\" to exit the Arcade\n\n")
 
         if playerchoice not in ["1", "2", "x"]:
             print(f"{name}, please enter 1, 2, or x.")
             return play_game(name)
 
         welcome_back = True
-
-        player = int(playerchoice)
 
         if (playerchoice == "1"):
             rock_paper_scissors = rps(name)

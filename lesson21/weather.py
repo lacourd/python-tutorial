4c624c8ec8f9ccba6d482a09c3f782e1c5a5bc21
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-10-18 11:58:01 -0500

    finished lesson21

--- lesson21/weather.py
+++ lesson21/weather.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from pprint import pprint
 
 load_dotenv()
 
@@ -11,6 +12,15 @@ def get_current_weather():
 
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
-    print(request_url)
+    # print(request_url)
 
-get_current_weather()
+    weather_data = requests.get(request_url).json()
+
+    # pprint(weather_data)
+
+    print(f'\nCurrent weather for {weather_data["name"]}')
+    print(f'\nThe temp is {weather_data["main"]["temp"]}')
+    print(f'\nFeels like {weather_data["main"]["feels_like"]} and {weather_data["weather"][0]["description"]}.\n')
+
+if __name__ == "__main__":
+    get_current_weather()

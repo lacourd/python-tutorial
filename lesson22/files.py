80f18e890f1e02fd797bcd6413495655fdd717fb
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-10-19 10:01:39 -0500

    updates

--- lesson22/files.py
+++ lesson22/files.py
@@ -54,3 +54,18 @@ f.close()
 if not os.path.exists("dave.txt"):
     f = open("dave.txt", "x")
     f.close()
+
+# Delete a file
+
+# avoid an error if it doesn't exist
+if os.path.exists("dave.txt"):
+    os.remove("dave.txt")
+else:
+    print("The file you wish to delete does not exist")
+
+
+with open("more_names.txt") as f:
+    content = f.read()
+
+with open("names.txt", "w") as f:
+    f.write(content)
